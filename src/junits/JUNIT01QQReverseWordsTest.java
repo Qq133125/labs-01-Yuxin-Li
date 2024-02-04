e52a51ddcfa057866a5b80293b2b0fa1174a81cd
YuxinLi <Yuxin.Li@seattlecolleges.edu>
2024-02-04 18:57:43 +0800

    update source for lab01

--- src/junits/JUNIT01QQReverseWordsTest.java
+++ src/junits/JUNIT01QQReverseWordsTest.java
@@ -1,48 +1,46 @@
 package junits;
 
-import lab01$arraylists.Test01ReverseWords;
-
-import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Scanner;
-
-import static org.junit.jupiter.api.Assertions.assertEquals;
+import lab01$arraylists.Test01ReverseWords;
+import org.junit.jupiter.api.Test;
 
 /**
- * @author A. Ford
+ * A. Ford
  * This is a JUnit Test for the Reverse Words Program
  * This class uses the Jupiter JUnit Test Library
  * current version is Jupiter 5.8.1
  */
 public class JUNIT01QQReverseWordsTest {
 
-    @Test
-    void testReverseWords() throws FileNotFoundException {
+  @Test
+  void testReverseWords() throws FileNotFoundException {
 
-        String matchLocation    = "." + File.separator + "data"
-                                      + File.separator + "match_words.txt";
+    String matchLocation = "." + File.separator + "data"
+        + File.separator + "match_words.txt";
 
-        String inputLocation    = "." + File.separator + "data"
-                                      + File.separator + "words.txt";
+    String inputLocation = "." + File.separator + "data"
+        + File.separator + "words.txt";
 
-        List<String> list       = new ArrayList<>();
+    List<String> list = new ArrayList<>();
 
-        Test01ReverseWords.read(inputLocation, list);
+    Test01ReverseWords.read(inputLocation, list);
 
-        Scanner scanner         = new Scanner(new File(matchLocation));
-        List<String> matchList  = new ArrayList<>(list.size());
+    Scanner scanner = new Scanner(new File(matchLocation));
+    List<String> matchList = new ArrayList<>(list.size());
 
-        while (scanner.hasNextLine()){
-            matchList.add(scanner.nextLine());
-        }
-
-        for (int i = 0; i < list.size(); i++) {
-            assertEquals(matchList.get(i), list.get(i));
-        }
+    while (scanner.hasNextLine()) {
+      matchList.add(scanner.nextLine());
     }
+
+    for (int i = 0; i < list.size(); i++) {
+      assertEquals(matchList.get(i), list.get(i));
+    }
+  }
 
 }
--- src/lab01$arraylists/Test01ReverseWords.java
+++ src/lab01$arraylists/Test01ReverseWords.java
@@ -2,125 +2,152 @@ package lab01$arraylists;
 
 import java.io.File;
 import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
 import java.io.PrintStream;
-
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Scanner;
 
 /**
- * @author A. Ford and You
+ * A. Ford and You
  * Create a runnable program with output as described in the .
  */
-
 public class Test01ReverseWords {
 
-    /**
-     * reads in an empty list and file location and populates list with
-     * the lines and words reversed.
-     * @param inputLocation for reading in the file information
-     * @param list to be populated
-     * @throws FileNotFoundException if file location not found
-     */
-    public static void read(String inputLocation, List<String> list) throws FileNotFoundException{
-        //TODO: check if input location exists
+  /**
+   * reads in an empty list and file location and populates list with
+   * the lines and words reversed.
+   *
+   * @param inputLocation for reading in the file information
+   * @param list          to be populated
+   * @throws FileNotFoundException if file location not found
+   */
+  public static void read(String inputLocation, List<String> list) throws FileNotFoundException {
 
-        //TODO: Create a File Object
+    //TODO: Create a File Object
+    File file = new File(inputLocation);
 
-        //TODO: Opens up a file input stream
+    //TODO: check if input location exists
+    validateInputLocation(inputLocation);
 
-        //TODO: store words in reverse in a list from each line
+    //TODO: Opens up a file input stream
+    //TODO: store words in reverse in a list from each line
+    //TODO: read in the data using input scanner
+    Scanner scanner = new Scanner(file);
 
-        //TODO: read in the data using input scanner
+    //TODO: read in next line and store as a string
+    while (scanner.hasNextLine()) {
+      StringBuilder stringBuilder = new StringBuilder();
+      String line = scanner.nextLine();
 
-            //TODO: read in next line and store as a string
+      //TODO: get each word on each line using a delimiter and store in index location
+      String[] lines = line.split(" ");
 
-            //TODO: get each word on each line using a delimiter and store in index location
+      for (String item : lines) {
+        //TODO: use string builder to build out lines with space
+        stringBuilder.insert(0, item + " ");
+      }
 
-            //TODO: store words in reverse
+      //TODO: store words in reverse
+      //TODO: add each reversed line to list as a string
+      list.add(0, stringBuilder.toString());
 
-            //TODO: use string builder to build out lines with space
+      //TODO: clear the words on each line
+      stringBuilder.delete(0, stringBuilder.length());
+    }
+  }
 
+  /**
+   * checks whether the file exists.
+   *
+   * @param location of file is checked
+   * @throws FileNotFoundException if file does not exist
+   */
+  public static void validateInputLocation(String location) throws FileNotFoundException {
 
-            //TODO: add each reversed line to list as a string
+    File inputFile = new File(location);
 
-            //TODO: clear the words on each line
-
-
+    if (!inputFile.exists()) {
+      throw new FileNotFoundException("File at " + location + " does not exist.");
     }
 
-    /**
-     * checks whether the file exists
-     * @param location of file is checked
-     * @throws FileNotFoundException if file does not exist
-     */
-    public static void validateInputLocation(String location) throws FileNotFoundException{
+    System.out.println("Processing \t" + location + "...");
 
-        File inputFile = new File(location);
+  }
 
-        if (!inputFile.exists()) {
-            throw new FileNotFoundException("File at " + location + " does not exist.");
-        }
+  /**
+   * checks whether the output directory exists.
+   *
+   * @param directory folder to place output
+   * @param location  for output file
+   */
+  public static void validateOutputFolder(String directory, String location) {
 
-        System.out.println("Processing \t" + location + "...");
-        
+    File outputFile = new File(location);
+
+    if (!outputFile.exists()) {
+
+      boolean hasCreatedDirectory = new File(directory).mkdir();
+      System.out.println("Has the directory been created? " + hasCreatedDirectory);
+      System.out.println();
+
+      if (hasCreatedDirectory) {
+        System.out.println("Processing \t" + directory + "...");
+      } else {
+        System.out.println("something went wrong.");
+      }
     }
 
-    /**
-     * checks whether the output directory exists
-     * @param directory folder to place output
-     * @param location for output file
-     */
-    public static void validateOutputFolder(String directory, String location) {
+  }
 
-        File outputFile = new File(location);
+  /**
+   * writes to an output file.
+   *
+   * @param outputLocation to where output file exists
+   * @param list           containing reversed words
+   * @throws FileNotFoundException when cannot write to output location
+   */
+  public static void write(String outputLocation, List<String> list) throws IOException {
 
-        if (!outputFile.exists()) {
+    //TODO: Open up a file output stream
+    FileOutputStream outputStream = new FileOutputStream(outputLocation);
+    PrintStream printStream = new PrintStream(outputStream);
 
-            boolean hasCreatedDirectory = new File(directory).mkdir();
-            System.out.println("Has the directory been created? " + hasCreatedDirectory);
-            System.out.println();
-
-            if (hasCreatedDirectory) {
-                System.out.println("Processing \t" + directory + "...");
-            }else {
-                System.out.println("something went wrong.");
-            }
-        }
-        
+    // TODO: write out lines using  Print Stream -> write
+    for (String item : list) {
+      printStream.println(item);
     }
 
-    /**
-     * writes to an output file
-     * @param outputLocation to where output file exists
-     * @param list containing reversed words
-     * @throws FileNotFoundException when cannot write to output location
-     */
-    public static void write(String outputLocation, List<String> list) throws FileNotFoundException{
-        
-        //TODO: Open up a file output stream
+    //TODO: close output stream
+    printStream.close();
+  }
 
-        // TODO: write out lines using  Print Stream -> write
+  /**
+   * main.
+   *
+   * @param args args
+   * @throws FileNotFoundException FileNotFoundException
+   */
+  public static void main(String[] args) throws IOException {
+    String inputLocation = "." + File.separator + "data"
+        + File.separator + "words.txt";
 
-        //TODO: close output stream
-    }
+    String outputFolder = "." + File.separator + "results";
 
+    String outputLocation = "." + File.separator + "results"
+        + File.separator + "reverse_words.txt";
 
-    public static void main(String[] args) throws FileNotFoundException {
-        String inputLocation    = "." + File.separator + "data"
-                                      + File.separator + "words.txt";
+    // TODO: update program to use an Array List
+    List<String> list = new ArrayList<>();
 
-        String outputFolder     = "." + File.separator + "results";
+    // TODO: process items in file (REVERSALS, can be done either in main or in read)
+    Test01ReverseWords.read(inputLocation, list);
 
-        String outputLocation   = "." + File.separator + "results"
-                                      + File.separator + "reverse_words.txt";
+    //TODO: Validate Output Directory Exists
+    validateOutputFolder(outputFolder, outputLocation);
 
-        // TODO: update program to use an Array List
-
-        // TODO: process items in file (REVERSALS, can be done either in main or in read)
-
-        //TODO: Validate Output Directory Exists
-
-    }
+    Test01ReverseWords.write(outputLocation, list);
+  }
 
 }
