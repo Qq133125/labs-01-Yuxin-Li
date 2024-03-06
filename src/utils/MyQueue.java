f39558bedeceb61accd068c69247cfdcfd6fbc3e
Yuxin Li <158444985+Qq133125@users.noreply.github.com>
2024-03-05 21:56:53 -0800

    Add files via upload

--- src/utils/MyQueue.java
+++ src/utils/MyQueue.java
@@ -1,4 +1,96 @@
 package utils;
 
-public class MyQueue {
+import java.util.NoSuchElementException;
+
+public class MyQueue<E> {
+
+    private static class Node<E> {
+        E data;
+        Node<E> next;
+
+        public Node(E data) {
+            this.data = data;
+            this.next = null;
+        }
+
+        public Node(E data, Node<E> next) {
+            this.data = data;
+            this.next = next;
+        }
+    }
+
+    private Node<E> first;
+    private Node<E> last;
+    private int size;
+
+    public MyQueue() {
+        this.first = null;
+        this.last = null;
+        this.size = 0;
+    }
+
+    public boolean add(E item) {
+        append(item);
+        size++;
+        return true;
+    }
+
+    private void append(E item) {
+        Node<E> newNode = new Node<>(item);
+        if (last == null) {
+            first = newNode;
+        } else {
+            last.next = newNode;
+        }
+        last = newNode;
+    }
+
+    private E detach() {
+        if (first == null) {
+            throw new NoSuchElementException();
+        }
+        E data = first.data;
+        first = first.next;
+        if (first == null) {
+            last = null;
+        }
+        return data;
+    }
+
+    public E remove() {
+        E data = detach();
+        size--;
+        return data;
+    }
+
+    public E peek() {
+        if (first == null) {
+            return null;
+        }
+        return first.data;
+    }
+
+    public int size() {
+        return size;
+    }
+
+    public boolean isEmpty() {
+        return size == 0;
+    }
+
+    @Override
+    public String toString() {
+        StringBuilder sb = new StringBuilder();
+        sb.append("[");
+        Node<E> current = first;
+        while (current != null) {
+            sb.append(current.data);
+            if (current.next != null) {
+                sb.append(", ");
+            }
+            current = current.next;
+        }
+        sb.append("]");
+        return sb.toString();
+    }
 }
--- src/utils/MyStack.java
+++ src/utils/MyStack.java
@@ -1,4 +1,82 @@
 package utils;
 
-public class MyStack {
+import java.util.EmptyStackException;
+import java.util.Stack;
+
+public class MyStack<E> {
+    private Node<E> first;
+    private int size;
+
+    public MyStack() {
+        first = null;
+        size = 0;
+    }
+
+    public E push(E item) {
+        Node<E> newNode = new Node<>(item, first);
+        first = newNode;
+        size++;
+        return item;
+    }
+
+    private E detach() {
+        if (isEmpty()) {
+            throw new EmptyStackException();
+        }
+        E data = first.data;
+        first = first.next;
+        size--;
+        return data;
+    }
+
+    public E pop() {
+        return detach();
+    }
+
+    public E peek() {
+        if (isEmpty()) {
+            throw new EmptyStackException();
+        }
+        return first.data;
+    }
+
+    public int size() {
+        return size;
+    }
+
+    public boolean isEmpty() {
+        return size == 0;
+    }
+
+    @Override
+    public String toString() {
+        StringBuilder sb = new StringBuilder();
+        sb.append("[");
+        Node<E> current = first;
+        Stack<E> tempStack = new Stack<>();
+        while (current != null) {
+            tempStack.push(current.data);
+            current = current.next;
+        }
+
+        while (!tempStack.isEmpty()) {
+            sb.append(tempStack.pop());
+            if (!tempStack.isEmpty()) {
+                sb.append(", ");
+            }
+        }
+
+        sb.append("]");
+        return sb.toString();
+    }
+
+    private static class Node<E> {
+        private E data;
+        private Node<E> next;
+
+        public Node(E data, Node<E> next) {
+            this.data = data;
+            this.next = next;
+        }
+    }
 }
